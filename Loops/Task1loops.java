a0125570ca325744207cb195107b3c97b906042f
ViktoriaProhorova <viktoriyaprokhorova7@gmail.com>
2024-03-10 23:30:34 +0100

    Loops Task 1

---
+++ Loops/Task1loops.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class Task1loops {
+    public static void main(String[] args) {
+        int number = 4;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(number + " x " + (i + 1) + " = " + (number * (i + 1)));
+        }
+    }
+}
