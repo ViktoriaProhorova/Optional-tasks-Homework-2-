5ba0f09bacda80573a05ebfc30d263303d1f2f5c
ViktoriaProhorova <viktoriyaprokhorova7@gmail.com>
2024-03-12 13:27:48 +0100

    Changed Task 1 - loops. Change: - "i+1" has been changed to "i", counter has been changed to "i <= 10" in accordance with the comment.

--- Loops/Task1loops.java
+++ Loops/Task1loops.java
@@ -2,9 +2,9 @@ package Loops;
 
 public class Task1loops {
     public static void main(String[] args) {
-        int number = 4;
-        for (int i = 0; i < 10; i++) {
-            System.out.println(number + " x " + (i + 1) + " = " + (number * (i + 1)));
+        int number = 5;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(number + " x " + (i) + " = " + (number * (i)));
         }
     }
 }
